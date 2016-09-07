a1cc1fd319cb6336b992d2fc4f51e88dd16d8441
fardanoleg <fardanoleg@gmail.com>
2016-09-06 19:38:30 -0700

    multifunctional calculator completed

--- script.js
+++ script.js
@@ -6,13 +6,7 @@ var input_position = 0;
 var button_text = null;
 var operator_text = null;
 var dot_text = null;
-var output = "";
-// var check_for_number = true;
-// var oper_array = ["-", "+", "/", "*"];
 var current_string = null;
-var equal_press_count = 0;
-var first_operator = null;
-var first_second_number = null;
 
 $(document).ready(function () {
 
@@ -42,18 +36,134 @@ function show_display() {
     $("#display").text(input_array[input_position]);
 }
 
-function check_operator_input_array() {
-    console.log("check operator function is running");
-    if (!isNaN(parseInt(input_array[0])) && ((input_array[1] == "+") || (input_array[1] == "-") || (input_array[1] == "/") || (input_array[1] == "*")) && !isNaN(parseFloat(input_array[2]))) {
-        console.log("IT IS TRUE check operator running", input_array);
-        show_display();
-        do_math();
-    } else {
-        return
+
+
+function check_array() {
+    console.log("checking  the array is running:");
+    for (var i = 0; i < input_array.length; i++) {
+        console.log("THE length for *,/ is: ", input_array.length);
+        if (input_array[i] == "*") {
+            console.log("Found *,/ will run it");
+            do_math();
+        } else if(input_array[i] == "/"){
+            do_math2();
+        }console.log(input_array);
     }
+    for (var i = 0; i < input_array.length; i++) {
+        console.log(" The length for +,-  is:", input_array.length);
+        if (input_array[i] == "-") {
+            console.log("Found -,+ will run it");
+            do_math3();
+        } else if (input_array[i] == "+"){
+            do_math4();
+        } console.log(input_array);
+    }
+    input_position = 0;
+    show_display();
 }
 
-function check_dot_input_array() {
+function do_math() {
+    console.log("do math is running");
+    var oper_index = input_array.indexOf("*");                     //index of the operator
+    var oper = input_array[oper_index];                               //the value on the oper
+    var num1 = parseFloat(input_array[oper_index - 1]);                              //the value of num1
+    var num2 = parseFloat(input_array[oper_index + 1]);                            //the value of num2
+    var num1_index = oper_index - 1;                              //index of num1
+    var num2_index = oper_index + 1;                              //index of num2
+    console.log("operator index: ", oper_index);
+    console.log("operator : ", oper);
+    console.log("the number before index: ", num1);
+    console.log("the number after index: ", num2);
+    console.log("the index of num1: ", num1_index);
+    console.log("the index of num2: ", num2_index);
+    var output = null;
+    switch (oper) {
+        case "*":
+            output = num1 * num2;
+            break;}
+    console.log(output);
+    num1 = output;
+    console.log(num1_index);
+    input_array.splice(num1_index, 3, String(num1));
+    check_array();
+    console.log("new_array: ", input_array);
+}
+
+function do_math2() {
+    console.log("do math2 is running");
+    var oper_index = input_array.indexOf("/");                     //index of the operator
+    var oper = input_array[oper_index];                            //the value on the oper
+    var num1 = parseFloat(input_array[oper_index - 1]);                              //the value of num1
+    var num2 = parseFloat(input_array[oper_index + 1]);                    //the value of num2
+    var num1_index = oper_index - 1;                              //index of num1
+    var num2_index = oper_index + 1;                              //index of num2
+    console.log("operator index: ", oper_index);
+    console.log("operator : ", oper);
+    console.log("the number before index: ", num1);
+    console.log("the number after index: ", num2);
+    console.log("the index of num1: ", num1_index);
+    console.log("the index of num2: ", num2_index);
+    var output = null;
+    switch (oper) {
+        case "/":
+            output = num1 / num2;
+            break;}
+    console.log(output);
+    num1 = output;
+    console.log(num1_index);
+    input_array.splice(num1_index, 3, String(num1));
+    check_array();
+    console.log("new_array: ", input_array);
+}
+
+function do_math3() {
+    console.log("do math is running");
+    var oper_index = input_array.indexOf("-");                     //index of the operator
+    var oper = input_array[oper_index];                            //the value on the oper
+    var num1 = parseFloat(input_array[oper_index - 1]);                              //the value of num1
+    var num2 = parseFloat(input_array[oper_index + 1]);                     //the value of num2
+    var num1_index = oper_index - 1;                              //index of num1
+    var num2_index = oper_index + 1;                              //index of num2
+    console.log("operator index: ", oper_index);
+    console.log("operator : ", oper);
+    console.log("the number before index: ", num1);
+    console.log("the number after index: ", num2);
+    console.log("the index of num1: ", num1_index);
+    console.log("the index of num2: ", num2_index);
+    var output = num1 - num2;
+    console.log(output);
+    num1 = output;
+    console.log(num1_index);
+    input_array.splice(num1_index, 3, String(num1));
+    check_array();
+    console.log("new_array: ", input_array);
+}
+
+function do_math4() {
+    console.log("do math is running");
+    var oper_index = input_array.indexOf("+");                     //index of the operator
+    var oper = input_array[oper_index];                            //the value on the oper
+    var num1 = parseFloat(input_array[oper_index - 1]);                              //the value of num1
+    var num2 = parseFloat(input_array[oper_index + 1]);                      //the value of num2
+    var num1_index = oper_index - 1;                              //index of num1
+    var num2_index = oper_index + 1;                              //index of num2
+    console.log("operator index: ", oper_index);
+    console.log("operator : ", oper);
+    console.log("the number before index: ", num1);
+    console.log("the number after index: ", num2);
+    console.log("the index of num1: ", num1_index);
+    console.log("the index of num2: ", num2_index);
+    var output = num1 + num2;
+    console.log(output);
+    num1 = output;
+    console.log(num1_index);
+    input_array.splice(num1_index, 3, String(num1));
+    check_array();
+    console.log("new_array: ", input_array);
+}
+
+
+function check_dot_input_array() {         //checks if the string has decimal already if true return true
     current_string = input_array[input_position];
     console.log("check for current string: ", current_string);
     for (var i = 0; i < current_string.length; i++) {
@@ -65,11 +175,11 @@ function check_dot_input_array() {
             console.log("you don't have any dots");
         }
     }
-    show_display()
+    show_display();
     return false
 }
 
-function press_dot(dot) {
+function press_dot(dot) {                         // checks if the string has decimal already using flag true, if it doesnt has decimal add one
     dot_text = $(dot).text();
     console.log("you clicked DOT: ", dot_text);
     check_dot_input_array();
@@ -77,6 +187,7 @@ function press_dot(dot) {
     if (result == false) {
         input_array[input_position] += dot_text;
         console.log(input_array[input_position]);
+
         console.log(input_array)
     } else {
         console.log("you have DOT!!!!");
@@ -85,10 +196,10 @@ function press_dot(dot) {
     show_display();
 }
 
-function press_number(num) {
+function press_number(num) {                   //  function when u press any number
     button_text = $(num).text();                        //assign the button to the right number
     console.log("you clicked number: ", button_text);
-    if (parseInt(input_array[input_position]) === 0 || input_array[input_position] == "ERROR") {
+    if (parseInt(input_array[input_position]) === 0 || input_array[input_position] == "ERROR") {  //set everything to 0
         input_array[input_position] = 0;
         input_array = [""];
     }
@@ -102,8 +213,8 @@ function press_number(num) {
 function press_operator(oper) {                                        //when you pres -,=,*,/
     operator_text = $(oper).text();                           //assign the button to the right number
     console.log("you clicked operator ", operator_text);
-    check_operator_input_array();
-    if (!isNaN(parseInt(input_array[input_position]))) {
+    // check_operator_input_array();                           //function to check if num1, num2 and operator exist aLREADY
+    if (!isNaN(parseFloat(input_array[input_position]))) {     // checks if the item before is a number
         console.log("the one before is a number");
         console.log(input_array[input_position]);
         input_position++;
@@ -113,7 +224,7 @@ function press_operator(oper) {                                        //when yo
         input_array[input_position] = "";            //assign the next string to be empty
         console.log(input_array);
 
-    } else {
+    } else {                                           //the item before is not a number to prevent situation like this +++++---**** etc...
         console.log("the one before is an operator");
         console.log("Array ", input_array);
         console.log("Position: ", input_position);
@@ -128,99 +239,6 @@ function press_operator(oper) {                                        //when yo
 
 }
 
-function do_math(num1, num2, oper) {
-    console.log("do_math is running");
-    var num1 = parseFloat(input_array[0]);
-    var num2 = parseFloat(input_array[2]);
-    console.log(num1, num2);
-    var oper = input_array[1];
-    first_second_number = num2;
-    first_operator = oper;
-    switch (oper) {
-        case "+" :
-            output = num1 + num2;
-            break;
-        case "-":
-            output = num1 - num2;
-            break;
-        case "*":
-            output = num1 * num2;
-            break;
-        case "/":
-            if (num2 == 0) {
-                console.log(" ERROR: ");
-                output = "ERROR"
-
-            } else {
-                output = num1 / num2;
-            }
-        default:
-            output = num1;
-
-    }
-    input_array = [""];
-    input_position = 0;
-    input_array[input_position] += output;
-    console.log(input_array);
-    show_display();
-}
-
-function do_math2(num1, oper) {
-    console.log("do_math2 is running");
-    var num1 = parseFloat(input_array[0]);
-    var oper = input_array[1];
-    console.log(num1, oper);
-    switch (oper) {
-        case "+" :
-            output = num1 + num1;
-            break;
-        case "-":
-            output = num1 - num1;
-            break;
-        case "*":
-            output = num1 * num1;
-            break;
-        case "/":
-            output = Math.sqrt(num1);
-            console.log(output);
-            break;
-    }
-    input_array = [""];
-    input_position = 0;
-    input_array[input_position] += output;
-    console.log(input_array);
-    show_display();
-}
-function do_math3(num1, num2, oper) {
-    console.log("do_math3 is running");
-    var num1 = parseFloat(input_array[0]);
-    var num2 = parseFloat(first_second_number);
-    var oper = first_operator;
-    console.log(num1, num2, oper);
-    switch (oper) {
-        case "+" :
-            output = num1 + num2;
-            break;
-        case "-":
-            output = num1 - num2;
-            break;
-        case "*":
-            output = num1 * num2;
-            break;
-        case "/":
-            if (num2 == 0) {
-                $("#display").text("Error");
-                output = ["Error"];
-            } else {
-                output = num1 / num2;
-            }
-    }
-    input_array = [""];
-    input_position = 0;
-    input_array[input_position] += output;
-    console.log(input_array);
-    show_display();
-}
 
 function press_clear() {                                     //reset everything
     console.log("you clicked reset");
@@ -230,35 +248,13 @@ function press_clear() {                                     //reset everything
     console.log(input_array);
 
 }
-function press_back() {
 
-}
 
-function press_equal(equal) {
+
+function press_equal(equal) {                  // situation when u press equal
     console.log("you pressed equal");
-    if (input_array[0] == "") {
-        input_array = [""];
-        input_position = 0;
-        equal_press_count++;
-        if (equal_press_count == 4) {
-            console.log("you pressed = 4 times");
-            $("#display").html("Ready");
-        }
-    }
-    else if (input_array[1] == undefined && first_second_number != null) {
-        console.log("DO_MATH3 started: ");
-        do_math3();
-    }
-    else if (input_array[2] == "") {
-        console.log("DO_MATH2 started: ");
-        do_math2();
-    } else {
-        console.log("DO_MATH started: ");
-        do_math();
-    }
-
+    check_array();
 }
-
 
 
 
